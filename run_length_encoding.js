fa61b457354680f56e2039c7f4c96aceb81b0d05
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2022-11-01 19:07:18 +0100

    first commit

--- run_length_encoding.js
+++ run_length_encoding.js
@@ -1,3 +1,21 @@
 function runLengthEncode() {
-    
+    const input = document.getElementById("fileInput");
+    const reader = new FileReader();
+    reader.onload = function() {
+        const img = new Image();
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const context = canvas.getContext("2d");
+            context.drawImage(img, 0, 0);
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            console.log(data.length);
+            document.body.appendChild(canvas);
+        }
+        img.src = reader.result;
+        //document.body.appendChild(img);
+    }
+    reader.readAsDataURL(input.files[0]);
 }
