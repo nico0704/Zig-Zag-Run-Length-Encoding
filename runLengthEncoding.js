3dd6603ec35873d922eaffd851419f3e0e57d11d
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2022-11-08 20:19:11 +0100

    code commented

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -6,7 +6,9 @@ var height;
 var width;
 
 function runLengthEncode() {
+    // init variables
     init();
+    // get file and project image on canvas to get image data
     const input = document.getElementById("fileInput");
     const reader = new FileReader();
     reader.onload = function () {
@@ -26,6 +28,7 @@ function runLengthEncode() {
             const data = imageData.data;
             console.log("Data-Length: " + data.length);
             console.log(data);
+            // store greyscale value for each pixel in array
             for (var i = 0; i < data.length; i += 4) {
                 let avg = parseInt((data[i] + data[i + 1] + data[i + 2]) / 3);
                 greyscale_arr.push(avg);
@@ -39,24 +42,30 @@ function runLengthEncode() {
             //
 
             // loop through the greyscale array and run-length-encode
+            // algorithm works for all image sizes 
             var y = 0;
             var x = 0;
+            // init prev_element with pixel in top left corner
             var prev_element = greyscale_arr[0];
             while (x < width && y < height) {
                 prev_element = encode(prev_element, x, y); // encode function will calc the index of current element (x + y * width)
                 if (x >= width - 1 && y >= height - 1) {
                     break; // end
                 }
+                // check if algorithm has to go down or right in the next step
                 if (x < width - 1) x++; // go right
                 else y++; // go down
+                // run through diagonal until x is 0 or y reaches height of the image
                 while (x > 0 && y < height - 1) {
                     prev_element = encode(prev_element, x, y);
                     x--;
                     y++;
                 }
                 prev_element = encode(prev_element, x, y);
+                // check if algorithm has to go down or right in the next step
                 if (y < height - 1) y++; // go down
                 else x++; // go right
+                // run through diagonal until y is 0 or x reaches width of the image
                 while (y > 0 && x < width - 1) {
                     prev_element = encode(prev_element, x, y);
                     y--;
@@ -73,9 +82,10 @@ function runLengthEncode() {
 }
 
 function encode(prev_element, x, y) {
+    // (x + y * width) calculates the index of the element in the greyscale array
     var pos = x + y * width;
     var cur_element = greyscale_arr[pos];
-    call_counter++;
+    call_counter++; // counts the number of times the encode function gets called
     if (pos == 0) {
         // first element
         element_counter++;
